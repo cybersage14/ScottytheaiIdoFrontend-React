071da31e82d80b99b2b5f392e2dacc0b2e2f2ed6
DarenGit28 <daren.shamoun28@gmail.com>
2023-06-21 14:40:26 +0900

    Footer

--- src/layouts/LandingLayout/Footer.tsx
+++ src/layouts/LandingLayout/Footer.tsx
@@ -1,7 +1,16 @@
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+// ---------------------------------------------------------------------------------------------
 
 export default function Footer() {
   return (
-    <Box component="footer"></Box>
+    <Box component="footer">
+      <Container>
+        <Typography textAlign="center" color={grey[100]} fontSize={14} py={3}>
+          © {new Date().getFullYear()} by Scotty Ai. All rights reserved!
+        </Typography>
+      </Container>
+    </Box>
   )
 }
