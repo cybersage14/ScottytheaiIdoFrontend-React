73b595ed3787814e46351d8154c3f8b24f9f5b44
DarenGit28 <daren.shamoun28@gmail.com>
2023-08-05 07:16:13 +0900

    proxy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "";
-
 const api = axios.create({
-  baseURL,
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json"
   }
