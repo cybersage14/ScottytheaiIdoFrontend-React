30d3aa88a5e079e5b9c04b8d771f3a7815a73959
DarenGit28 <daren.shamoun28@gmail.com>
2023-08-11 23:55:09 +0900

    Tokenomics

--- src/pages/Home/HowToBuySection.tsx
+++ src/pages/Home/HowToBuySection.tsx
@@ -1,5 +1,5 @@
-import { Box, Container, Grid, LinearProgress, Stack, Typography, useTheme } from "@mui/material";
-import { grey, orange } from "@mui/material/colors";
+import { Box, Container, Grid, Stack, Typography, useTheme } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { Icon } from "@iconify/react";
 import SectionTitle from "../../components/SectionTitle";
 
@@ -41,68 +41,56 @@ import SectionTitle from "../../components/SectionTitle";
 const TOKENOMICS = [
   {
     id: 1,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Private Round',
+    tokenAmount: '54,567,890',
     status: 2
   },
   {
     id: 2,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Public 1',
+    tokenAmount: '150,000,000',
     status: 2
   },
   {
     id: 3,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Public 2',
+    tokenAmount: '150,000,000',
     status: 2
   },
   {
     id: 4,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Public 3',
+    tokenAmount: '180,000,000',
     status: 2
   },
   {
     id: 5,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Public 4',
+    tokenAmount: '200,000,000',
     status: 2
   },
   {
     id: 6,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Public 5',
+    tokenAmount: '200,000,000',
     status: 2
   },
   {
     id: 7,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Exchange Listings',
+    tokenAmount: '123,456,789',
     status: 2
   },
   {
     id: 8,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
-    status: 2
-  },
-  {
-    id: 9,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
-    status: 2
-  },
-  {
-    id: 10,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    label: 'Scotty Development',
+    tokenAmount: '123,456,789',
     status: 1
   },
   {
-    id: 11,
-    label: 'Public Pool 30%',
-    tokenAmount: '3,000,000,000',
+    id: 9,
+    label: 'Burned by Scotty',
+    tokenAmount: '53,086,422',
     status: 0
   }
 ]
@@ -114,7 +102,7 @@ export default function HowToBuySection() {
 
   return (
     <Box component="section" py={{ xs: 6, md: 12 }}>
-      <Container sx={{ display: 'flex', flexDirection: 'column', gap: { xs: 8, md: 16 } }}>
+      <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', gap: { xs: 8, md: 16 } }}>
         {/* How to buy */}
         {/* <Box>
           <SectionTitle variant="h2" sx={{ textAlign: 'center' }}>How to Buy?</SectionTitle>
@@ -138,24 +126,24 @@ export default function HowToBuySection() {
           <Box>
             <Grid container spacing={{ xs: 4, md: 8 }} alignItems="center">
               <Grid item xs={12} md={4}>
-                <Stack alignItems="center">
-                  <Typography component="span" fontWeight={700} fontSize={{ xs: 32, md: 48 }} color={grey[100]}>90%</Typography>
-                  <Typography fontSize={14} color={grey[100]}>Supply to Liquidity Pool = 1111111101</Typography>
+                <Stack alignItems="center" spacing={1}>
+                  <Typography component="span" fontWeight={700} fontSize={{ xs: 32, md: 48 }} color={grey[100]}>75%</Typography>
+                  <Typography fontSize={14} color={grey[100]}>Public Sale = 934,567,890</Typography>
                 </Stack>
               </Grid>
 
               <Grid item xs={12} md={4}>
-                <LinearProgress
-                  value={90}
-                  variant="determinate"
-                  sx={{ height: 56, borderRadius: 9999 }}
-                />
+                <Stack height={48} direction="row" alignItems="stretch" width="100%" bgcolor={grey[900]} borderRadius={9999}>
+                  <Box bgcolor={theme.palette.primary.main} width="75%" sx={{ borderTopLeftRadius: 9999, borderBottomLeftRadius: 9999 }} />
+                  <Box bgcolor="#DC640C" width="9%" />
+                  <Box bgcolor="#793300" width="9%" />
+                </Stack>
               </Grid>
 
               <Grid item xs={12} md={4}>
-                <Stack alignItems="center">
-                  <Typography component="span" fontWeight={700} fontSize={{ xs: 32, md: 48 }} color={grey[100]}>10%</Typography>
-                  <Typography fontSize={14} color={grey[100]} textAlign="center">Supply for Exchange Listings = 123456789</Typography>
+                <Stack alignItems="center" spacing={1}>
+                  <Typography component="span" fontWeight={700} fontSize={{ xs: 32, md: 48 }} color={grey[100]}>25%</Typography>
+                  <Typography fontSize={14} color={grey[100]} textAlign="center">Exchange Listings, Development & Burn = 123456789</Typography>
                 </Stack>
               </Grid>
             </Grid>
@@ -176,9 +164,9 @@ export default function HowToBuySection() {
             </Grid>
 
             <Grid item xs={12} md={6}>
-              <Stack spacing={4}>
+              <Stack spacing={4} alignItems={{ xs: 'center', md: 'start' }}>
                 <SectionTitle sx={{ textAlign: { xs: 'center', md: 'left' } }}>Tokenomics</SectionTitle>
-                <Box>
+                <Box display={{ xs: 'none', md: 'block' }}>
                   <Grid container spacing={4}>
                     {TOKENOMICS.map(tokenomic => (
                       <Grid key={tokenomic.id} item xs={12} md={6}>
@@ -188,7 +176,7 @@ export default function HowToBuySection() {
                             icon="mdi:paw"
                             sx={{
                               fontSize: 36,
-                              color: tokenomic.status === 2 ? theme.palette.primary.main : tokenomic.status === 1 ? orange[900] : grey[900],
+                              color: tokenomic.status === 2 ? theme.palette.primary.main : tokenomic.status === 1 ? '#793300' : grey[800],
                             }}
                           />
                           <Stack>
@@ -200,6 +188,25 @@ export default function HowToBuySection() {
                     ))}
                   </Grid>
                 </Box>
+
+                <Stack width="fit-content" spacing={3} display={{ xs: 'flex', md: 'none' }}>
+                  {TOKENOMICS.map(tokenomic => (
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                      <Box
+                        component={Icon}
+                        icon="mdi:paw"
+                        sx={{
+                          fontSize: 36,
+                          color: tokenomic.status === 2 ? theme.palette.primary.main : tokenomic.status === 1 ? '#793300' : grey[800],
+                        }}
+                      />
+                      <Stack>
+                        <Typography component="span" color={grey[100]} fontSize={16}>{tokenomic.label}</Typography>
+                        <Typography component="span" color={theme.palette.primary.main} fontSize={16}>{tokenomic.tokenAmount}</Typography>
+                      </Stack>
+                    </Stack>
+                  ))}
+                </Stack>
               </Stack>
             </Grid>
           </Grid>
